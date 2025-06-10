1c4f6ec07120be3e5ff423264db68443af547adb
haseebr7 <haseeeburreyan@gmail.com>
2025-06-10 16:20:33 +0500

    Updated print statement

--- Control flow and if else crab.py
+++ Control flow and if else crab.py
@@ -1,6 +1,17 @@
-Number = int(input("Enter the number to check if it's even: "))
+# 2nd part
 
-if Number % 2 == 0:
-    print("It's even")
+weight = 85
+height = 1.85
+
+bmi = weight / (height ** 2)
+
+
+print(bmi)
+if bmi <= 18.5:
+    print("Under weight")
+elif 18.5 < bmi < 24.9:
+    print("Normal")
+elif 25 < bmi < 29.9:
+    print("Over weight")
 else:
-    print("It's odd")
+    print("Evacuate this person, NOW!!!")
