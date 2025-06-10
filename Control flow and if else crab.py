9d4c065d94fa3e73c2360a87990c7aa0d089d7d2
haseebr7 <haseeeburreyan@gmail.com>
2025-06-10 18:45:20 +0500

    Updated print statement

--- Control flow and if else crab.py
+++ Control flow and if else crab.py
@@ -1,17 +1,29 @@
-# 2nd part
+# 3rd part
+print("Welcome to this Roller Coster Ride")
+Bill=0
+height = int(input("What's your height? \n"))
 
-weight = 85
-height = 1.85
+if height >= 120:
+    age = int(input("Good! You can ride but What's your age? \n"))
+    if age < 12:
+        print("Your ticket is $5")
+        Bill += 5
+    elif 12 >= age <=18:
+        print("Your ticket is $7")
+        Bill += 7
+    elif age > 18:
+        print("Your ticket is $12")
+        Bill += 12
 
-bmi = weight / (height ** 2)
+    Photo = input("Do you want photos as well? It would cost $3 extra. Y/N\n")
 
+    if Photo == "Y":
+        Bill += 3
 
-print(bmi)
-if bmi <= 18.5:
-    print("Under weight")
-elif 18.5 < bmi < 24.9:
-    print("Normal")
-elif 25 < bmi < 29.9:
-    print("Over weight")
+    elif Photo == "N":
+        Bill += 0
+
+    print(f"Ok \nYour total bill is ${Bill}\nEnjoy the Ride.")
+
 else:
-    print("Evacuate this person, NOW!!!")
+    print("You can't ride! You better grow up.")
