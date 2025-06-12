db9d8e6fc0de424e08ceb31e6aba73e07f10e019
haseebr7 <haseeeburreyan@gmail.com>
2025-06-12 06:33:10 +0500

    initial commit

---
+++ Max number finder.py
@@ -0,0 +1,9 @@
+Vari = [4111,44,4,56,45,6,5,5,5,1,21,2111,2,21,5,31,5,153,11,511,888855,5,545,5,5,45,5,6,65,64,64,65]
+Our_List = 0
+for i in Vari:
+    if i >= Our_List:
+        Our_List = i
+        
+print(Our_List)
+
+#I nailed it, didn't i?
