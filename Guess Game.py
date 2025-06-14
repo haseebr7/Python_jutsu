467ebfc45703c8c3fda9dc7976e6a05776ed3da8
haseebr7 <haseeeburreyan@gmail.com>
2025-06-14 05:57:52 +0500

    initial commit

--- Guess Game.py
+++ Guess Game.py
@@ -1,9 +1,16 @@
 import random
 Words = [
-    "Titan", "Shinobi", "Sharingan", "Konoha", "AoT", "Chidori", "Mangekyo", "Donghua", "TimeTravel", "LinkClick",
-    "Ackerman", "Naruto", "Kurama", "Shippuden", "Paradis", "Reiner", "Sasuke", "Erwin", "Minato", "Xiang"
+    "titan", "shinobi", "sharingan", "konoha", "aot", "chidori", "mangekyo", "donghua", "timetravel", "linkclick",
+    "ackerman", "naruto", "kurama", "shippuden", "paradis", "reiner", "sasuke", "erwin", "minato", "xiang"
 ]
+
 
 word = random.choice(Words)
 
 print(word)
+Guessed_letter = input("Guess a letter: ").lower()
+for i in word:
+    if i == Guessed_letter:
+        print("Right")
+    else:
+        print("Wrong")
