768b7e6db23aba948f33851cd9736ff98094b4e3
haseebr7 <haseeeburreyan@gmail.com>
2025-06-14 05:38:26 +0500

    initial commit

---
+++ Function stuff.py
@@ -0,0 +1,33 @@
+#IGNORE IT DUDE
+
+
+
+def turn_left():
+    
+
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    while front_is_clear():
+        move()
+    turn_right()
+
+
+while not at_goal():
+    if wall_in front():
+        jump()
+    else:
+        move()
---
+++ Guess Game.py
@@ -0,0 +1,9 @@
+import random
+Words = [
+    "Titan", "Shinobi", "Sharingan", "Konoha", "AoT", "Chidori", "Mangekyo", "Donghua", "TimeTravel", "LinkClick",
+    "Ackerman", "Naruto", "Kurama", "Shippuden", "Paradis", "Reiner", "Sasuke", "Erwin", "Minato", "Xiang"
+]
+
+word = random.choice(Words)
+
+print(word)
