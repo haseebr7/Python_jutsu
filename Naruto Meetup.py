498d3a3d1e87a89a1fb14abe2421dbab19933400
haseebr7 <haseeeburreyan@gmail.com>
2025-06-16 08:39:55 +0500

    initial commit

--- Naruto Meetup.py
+++ Naruto Meetup.py
@@ -3,4 +3,4 @@ def greet(name, rank, jutsu):
     print(f"So you are {rank} now, huh... So cool!")
     print(f"Well, i have a favour to ask can you teach me {jutsu}.. Please")
 
-greet(name="Naruto", rank="jonin", jutsu="Rasen shurikin")
+greet(name="Sasuke", rank="Jonin", jutsu="Chidori")
