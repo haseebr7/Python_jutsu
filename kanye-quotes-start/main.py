6f0aa78509448a38d61e355b5d17fd535b72a183
haseebr7 <haseeeburreyan@gmail.com>
2025-10-10 18:21:53 +0500

    initial_commit

---
+++ kanye-quotes-start/main.py
@@ -0,0 +1,34 @@
+from tkinter import *
+import requests
+quote = ""
+
+def get_quote():
+    global quote
+
+    quote_api = requests.get("https://api.kanye.rest/")
+    quote_api.raise_for_status()
+    quote = quote_api.json()["quote"]
+    canvas.itemconfig(quote_text,text=quote)
+
+    print(quote)
+
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+kanye_img = PhotoImage(file="kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="quote", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+
+
+
+
+window.mainloop()
